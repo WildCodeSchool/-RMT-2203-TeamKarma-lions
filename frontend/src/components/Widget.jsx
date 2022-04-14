c789c39b920945587eb0e0a90117ab141e31d369
FREDERIQUE DEMAS <frederique.demas73@gmail.com>
2022-04-14 10:11:13 +0200

    first display widgetlists

--- frontend/src/components/Widget.jsx
+++ frontend/src/components/Widget.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Widget() {
-  return <div className="widget">Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet</div>;
+  return (
+    <div className="widget">
+      Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit
+      ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet
+    </div>
+  );
 }
