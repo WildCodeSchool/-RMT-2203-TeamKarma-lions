e76c0bfbc85bc7709b9109db8f1f3a52b09674d6
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-04-14 16:03:17 +0200

    add 2 axios promess

--- frontend/src/components/FunFact.jsx
+++ frontend/src/components/FunFact.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function FunFact({ textFront, factData, textBack }) {
+export default function FunFact({ textFront, factData }) {
   return (
     <>
-      {textFront} {factData} {textBack}
+      {textFront} {factData}
     </>
   );
 }
--- frontend/src/components/FunFactList.jsx
+++ frontend/src/components/FunFactList.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import FunFact from "./FunFact";
 
 export default function FunFactList() {
+  const [marsTemp, setMarsTemp] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://cab.inta-csic.es/rems/wp-content/plugins/marsweather-widget/api.php`
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setMarsTemp(data.soles[0].max_temp);
+      });
+  }, []);
+
+  const [nbExo, setNbExo] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+count(releasedate)+from+ps+where+default_flag=1&format=json`
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setNbExo(data[0]["count(releasedate)"]);
+      });
+  }, []);
+
   const funFacts = [
     {
-      textFront: "la température sur Mars est de",
-      textBack: "°C",
-      factData: "666",
+      text: "la température maximum sur Mars aujourdhui est de",
+      factData: `${marsTemp} C°`,
     },
 
     {
-      textFront: "front B",
-      textBack: "back B",
-      factData: "Data B",
+      text: "Nombre d'exoplanètes découvertes:",
+      factData: nbExo,
     },
 
     {
-      textFront: "front C",
-      textBack: "back C",
-      factData: "Data C",
+      text: "la température sur Saturne est de",
+      factData: 666,
     },
   ];
 
@@ -27,9 +51,8 @@ export default function FunFactList() {
       {funFacts.map((funFact) => (
         <FunFact
           key={`${funFact.factData}`}
-          textFront={funFact.textFront}
+          textFront={funFact.text}
           factData={funFact.factData}
-          textBack={funFact.textBack}
         />
       ))}
     </>
