3e736d4e0830dcd97907828d6fc1e2e01a8b5a84
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-04-19 16:33:58 +0200

    added epic page structure

---
+++ frontend/src/pages/Epic.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/Home.scss";
+import Titre from "../components/Titre";
+import Calendar from "../components/Epic/Calendar";
+import EpicEvent from "../components/Epic/EpicEvent";
+import EpicPictures from "../components/Epic/EpicPictures";
+
+export default function Home() {
+  return (
+    <>
+      <Titre titre="DSCOVR : EPIC" />
+      <p className="intro-site">
+        Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam
+        haut ita dudum alimentorum inopiam pellerentur ab urbe praecipites,
+        sectatoribus disciplinarum liberalium inpendio paucis sine respiratione
+        ulla extrusis, tenerentur minimarum adseclae veri, quique id simularunt
+        ad tempus, et tria milia saltatricum ne interpellata quidem cum choris
+        totidemque remanerent magistris.
+      </p>
+      <Calendar />
+      <EpicEvent />
+      <EpicPictures />
+    </>
+  );
+}
