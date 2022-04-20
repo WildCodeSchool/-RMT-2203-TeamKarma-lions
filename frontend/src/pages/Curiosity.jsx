ee0522e19bc4591390f9453b5a7e5f4a501f65ff
ClemDSC <clemence.p14@gmail.com>
2022-04-20 11:25:51 +0200

    creation route : Curiosity and his page

---
+++ frontend/src/pages/Curiosity.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Home.scss";
+import Titre from "../components/Titre";
+
+export default function Home() {
+  return (
+    <>
+      <Titre titre="Rover : CURIOSITY" />
+      <p className="intro-site">
+        Curiosity is a rover created by JPL, one of NASAs research centers. Its
+        purpose is to explore Mars, as part of the Mars Science Laboratory
+        mission. This rover landed on Mars on August 6, 2012 and is still on the
+        red planet.
+      </p>
+    </>
+  );
+}
