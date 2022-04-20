44bf63de31c459eb1027a1aa64fa102ca8bf4f76
Lora Perrichon <loraperrichon@gmail.com>
2022-04-20 10:46:14 +0200

    hover avec classname et state sur div onglet

--- frontend/src/components/ItemNavbar.jsx
+++ frontend/src/components/ItemNavbar.jsx
@@ -3,13 +3,9 @@ import "../styles/Navbar.scss";
 
 export default function ItemNavbar({ image, alt, isActive }) {
   const [active, setActive] = useState({ isActive });
-  const [activeOnglet, setActiveOnglet] = useState({ isActive });
   const handleActive = () => {
     setActive(!active);
     // ajout d'une classe active quand on passe la souris sur l'élément
-  };
-  const handleActiveOnglet = () => {
-    setActiveOnglet(!activeOnglet);
   };
   return (
     <div className={active ? "isActive" : "notActive"}>
@@ -21,7 +17,7 @@ export default function ItemNavbar({ image, alt, isActive }) {
         src={image}
         alt={alt}
       />
-      <div className={activeOnglet ? "isActiveOnglet" : "isNotActiveOnglet"}>
+      <div>
         <h3 className="texteOnglet">{alt}</h3>
       </div>
     </div>
