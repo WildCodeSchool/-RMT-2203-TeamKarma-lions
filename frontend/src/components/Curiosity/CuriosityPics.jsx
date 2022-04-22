3a17574158bd3918d80932cd24e6b3a1e82fb6cf
ClemDSC <clemence.p14@gmail.com>
2022-04-22 11:01:00 +0200

    insert pics from api

--- frontend/src/components/Curiosity/CuriosityPics.jsx
+++ frontend/src/components/Curiosity/CuriosityPics.jsx
@@ -1,25 +1,37 @@
-// import React from "react";
-// import axios from "axios";
+import React, { useEffect } from "react";
+import axios from "axios";
+import "../../styles/CuriosityPics.scss";
+import PicCard from "./PicCard";
 
-// export default function CuriosityPics() {
-//   const [picList, setPicList] = React.useState();
+export default function CuriosityPics() {
+  const [picList, setPicList] = React.useState([]);
 
-//   const getPic = () => {
-//     axios
-//       .get(
-//         "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2022-4-10&api_key=DEMO_KEY"
-//       )
-//       .then((response) => response.data)
-//       .then((data) => {
-//         setPicList(data);
-//       });
-//   };
+  const getPic = () => {
+    axios
+      .get(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2022-4-10&api_key=ld3yHUvzO7DyXL6lzXi5jSTRd4bGpdfKgBGpjNUg"
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        // console.log(data.photos);
+        setPicList(data.photos);
+      });
+  };
 
-//   return (
-//     <div className="CuriosityPics">
-//       <button type="button" onClick={getPic}>
-//         Click to see random photo
-//       </button>
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    getPic();
+  }, []);
+
+  return (
+    <div className="containerPic">
+      <button type="button" onClick={getPic}>
+        Click to see random photo
+      </button>
+      <div className="gridpics">
+        {picList.map((pic, index) =>
+          index < 20 ? <PicCard pic={pic} key={pic.id} /> : null
+        )}
+      </div>
+    </div>
+  );
+}
