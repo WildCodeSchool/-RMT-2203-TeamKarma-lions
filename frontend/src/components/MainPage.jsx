75d4af4c6de54e3050e63cab38883970216d7dd3
FREDERIQUE DEMAS <frederique.demas73@gmail.com>
2022-04-22 23:00:57 +0200

    merge conflicts

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
 import Home from "../pages/Home";
 import Epic from "../pages/Epic";
 import Footer from "./Footer";
 
 export default function MainPage() {
   return (
-    <div className="mainpage">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Epic" element={<Epic />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router>
+      <div className="mainpage">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Epic" element={<Epic />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
