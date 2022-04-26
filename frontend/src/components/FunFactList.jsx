30e1ff897db238351f7353ae80d103820289c599
gnos28 <gnos28@gmail.com>
2022-04-26 16:05:30 +0200

    icons & number animations

--- frontend/src/components/FunFactList.jsx
+++ frontend/src/components/FunFactList.jsx
@@ -4,7 +4,9 @@ import axios from "axios";
 import FunFact from "./FunFact";
 
 export default function FunFactList() {
-  const [marsTemp, setMarsTemp] = useState("LOAD");
+  const [marsTemp, setMarsTemp] = useState([0, 0]);
+  const [nbExo, setNbExo] = useState(0);
+  const [fireBallImpact, setFireBallImpact] = useState(0);
 
   useEffect(() => {
     axios
@@ -13,11 +15,14 @@ export default function FunFactList() {
       )
       .then((response) => response.data)
       .then((data) => {
-        setMarsTemp(`${data.soles[0].max_temp} °C`);
+        console.log("setMarsTemp", data, data.soles[0].max_temp);
+
+        setMarsTemp([
+          parseInt(data.soles[0].min_temp, 10),
+          parseInt(data.soles[0].max_temp, 10),
+        ]);
       });
   }, []);
-
-  const [nbExo, setNbExo] = useState("LOAD");
 
   useEffect(() => {
     axios
@@ -26,38 +31,52 @@ export default function FunFactList() {
       )
       .then((response) => response.data)
       .then((data) => {
+        console.log("setNbExo", data);
+
         setNbExo(data[0]["count(releasedate)"]);
       });
   }, []);
-
-  const [fireBallImpact, setFireBallImpact] = useState("LOAD");
 
   useEffect(() => {
     axios
       .get(`https://ssd-api.jpl.nasa.gov/fireball.api?limit=1`)
       .then((response) => response.data)
       .then((data) => {
-        setFireBallImpact(`${data.data["0"][2]} kt`);
+        console.log("setFireBallImpact", data);
+        setFireBallImpact(data.data["0"][2]);
       });
   }, []);
 
   const funFacts = [
     {
-      text1: "Max Temp for Curiosity :",
-      factData: marsTemp,
-      text2: "",
+      text1: "Mars weather :",
+      factData: marsTemp[0],
+      factData2: marsTemp[1],
+      text2: " / ",
+      text3: " °C",
+      start: 30,
+      round: 0,
+      icon: "sun.png",
     },
 
     {
-      text1: "Exoplanets discovered :",
+      text1: "Exoplanets count :",
       factData: nbExo,
+      factData2: null,
       text2: "",
+      start: 0,
+      round: 0,
+      icon: "orbit.png",
     },
 
     {
-      text1: "Impact of the last Fireball :",
+      text1: "Last fireball impact :",
       factData: fireBallImpact,
-      text2: "",
+      factData2: null,
+      text2: " kt",
+      start: 0,
+      round: 2,
+      icon: "meteor.png",
     },
   ];
 
@@ -68,7 +87,12 @@ export default function FunFactList() {
           key={`${funFact.text1}`}
           textFront={funFact.text1}
           factData={funFact.factData}
+          factData2={funFact.factData2}
           textBack={funFact.text2}
+          textBack2={funFact.text3}
+          dataStart={funFact.start}
+          dataRound={funFact.round}
+          icon={funFact.icon}
         />
       ))}
     </div>
