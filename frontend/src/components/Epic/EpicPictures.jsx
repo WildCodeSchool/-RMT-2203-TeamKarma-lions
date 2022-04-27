4b9c2c8c06e5b74a87748cf5203d3b65e77bab3e
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-04-27 12:16:16 +0200

    add .map with img links

--- frontend/src/components/Epic/EpicPictures.jsx
+++ frontend/src/components/Epic/EpicPictures.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import moment from "moment";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
 
 export default function EpicPictures({ epicPicture }) {
   if ({ epicPicture }.epicPicture) {
@@ -19,7 +16,7 @@ export default function EpicPictures({ epicPicture }) {
 
     const fetchEpicPictures = (date) => {
       axios
-        .get(`https://epic.gsfc.nasa.gov/api/enhanced/date/${date}`)
+        .get(`https://epic.gsfc.nasa.gov/api/natural/date/${date}`)
         .then((response) => response.data)
         .then((data) => {
           if (data.length === 0) {
@@ -28,7 +25,6 @@ export default function EpicPictures({ epicPicture }) {
           } else {
             const data2 = data.map((element) => element.image);
             data2.unshift(moment(date, "YYYY-MM-DD").format("YYYY/MM/DD"));
-            return data2;
           }
         });
     };
@@ -37,5 +33,34 @@ export default function EpicPictures({ epicPicture }) {
       fetchEpicPictures(picturesDate);
     }, [epicPicture]);
   }
-  return <div>test</div>;
+
+  const picsdate = "2022/04/13";
+
+  const data2 = [
+    "epic_1b_20220413001751",
+    "epic_1b_20220413020553",
+    "epic_1b_20220413035355",
+    "epic_1b_20220413054158",
+    "epic_1b_20220413073000",
+    "epic_1b_20220413091802",
+    "epic_1b_20220413110605",
+    "epic_1b_20220413125408",
+    "epic_1b_20220413144210",
+    "epic_1b_20220413163013",
+    "epic_1b_20220413181814",
+    "epic_1b_20220413200617",
+    "epic_1b_20220413215419",
+  ];
+
+  return (
+    <div>
+      {data2.map((each) => (
+        <img
+          className="lazy"
+          src={`https://epic.gsfc.nasa.gov/archive/natural/${picsdate}/png/${each}.png`}
+          alt="sample"
+        />
+      ))}
+    </div>
+  );
 }
