c3c27d167d84b3797dbeba39d1ddffe6bddc1299
ClemDSC <clemence.p14@gmail.com>
2022-04-27 10:57:16 +0200

    update timeline

--- frontend/src/pages/Curiosity.jsx
+++ frontend/src/pages/Curiosity.jsx
@@ -9,13 +9,14 @@ export default function Home() {
   return (
     <>
       <Titre titre="Rover : CURIOSITY" />
+      <Timeline />
       <p className="intro-site">
         Curiosity is a rover created by JPL, one of NASAs research centers. Its
         purpose is to explore Mars, as part of the Mars Science Laboratory
         mission. This rover landed on Mars on August 6, 2012 and is still on the
         red planet.
       </p>
-      <Timeline />
+
       <CountCuriosity />
       <CuriosityPics />
     </>
