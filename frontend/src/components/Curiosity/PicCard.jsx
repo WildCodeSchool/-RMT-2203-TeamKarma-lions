4b5888af7f4dc5a08208036dba0d81dff859e846
ClemDSC <clemence.p14@gmail.com>
2022-04-27 20:43:42 +0200

    update colors css calendar // open rover pic in another window

--- frontend/src/components/Curiosity/PicCard.jsx
+++ frontend/src/components/Curiosity/PicCard.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function PicCard({ pic }) {
   return (
     <div className="alonecard">
-      <img src={pic.img_src} alt="planet" />
+      <a href={pic.img_src} target="_blank" rel="noreferrer">
+        <img src={pic.img_src} alt="planet" />
+      </a>
+      <p>Camera : {pic.camera.name}</p>
     </div>
   );
 }
