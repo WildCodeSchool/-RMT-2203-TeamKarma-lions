12a85f5bfe539d01d77b4419e782d28ee4a7e348
ClemDSC <clemence.p14@gmail.com>
2022-04-29 12:32:34 +0200

    change homme.scss

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,10 +3,51 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="lionsfooter">
-      <p>
+      <ul>
+        <li>
+          <p className="introFooter">2022 © Wild Code School x Team Lion :</p>
+        </li>
+        <li>
+          <a href="https://github.com/gnos28">
+            {" "}
+            <img src="./src/assets/GitHubPic.png" alt="logo Github" />
+          </a>
+
+          <p>Julien Vigneron,</p>
+        </li>
+        <li>
+          <a href="https://github.com/ClemDSC">
+            {" "}
+            <img src="./src/assets/GitHubPic.png" alt="logo Github" />
+          </a>
+          <p>Clémence Pham,</p>
+        </li>
+        <li>
+          <a href="https://github.com/Lora048">
+            {" "}
+            <img src="./src/assets/GitHubPic.png" alt="logo Github" />
+          </a>
+          <p>Lora Perrichon,</p>
+        </li>
+        <li>
+          <a href="https://github.com/FrederiqueDemas">
+            {" "}
+            <img src="./src/assets/GitHubPic.png" alt="logo Github" />
+          </a>
+          <p>Frédérique Demas,</p>
+        </li>
+        <li>
+          <a href="https://github.com/PaulKip44">
+            {" "}
+            <img src="./src/assets/GitHubPic.png" alt="logo Github" />
+          </a>
+          <p>Paul Sanchez.</p>
+        </li>
+      </ul>
+      {/* <p>
         2022 © Wild Code School x Team Lion : Julien Vigneron, Clémence Pham,
         Lora Perrichon, Frédérique Demas, Paul Sanchez.
-      </p>
+      </p> */}
     </div>
   );
 }
