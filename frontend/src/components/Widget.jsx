5c8ec6210208efd014a3a206da947e957cc66dfc
Lora Perrichon <loraperrichon@gmail.com>
2022-05-01 20:11:31 +0200

    ajustement composant widget + mise en place mapping (tests)

--- frontend/src/components/Widget.jsx
+++ frontend/src/components/Widget.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "../styles/Widget.scss";
 import "../styles/App.scss";
 
-export default function Widget() {
+export default function Widget({ logo, name, image, link }) {
   return (
     <div className="widget">
-      <img src="../src/assets/satellite.png" alt="satellite" />
-      <h3>Curiosity Persévérance</h3>
+      <NavLink to={link}>
+        <div className="logoWidget">
+          <img src={logo} alt={name} />
+        </div>
+        <h3>{name}</h3>
+        <div className="imageWidget">
+          <img src={image} alt={name} />
+        </div>
+      </NavLink>
     </div>
   );
 }
--- frontend/src/components/WidgetList.jsx
+++ frontend/src/components/WidgetList.jsx
@@ -4,9 +4,29 @@ import Widget from "./Widget";
 import "../styles/Widget.scss";
 
 export default function WidgetList() {
+  const contenuWidget = [
+    {
+      name: "Epic",
+      logo: "../src/assets/perseverance.png",
+      link: "/Epic",
+      image: "../src/assets/lion-logo.png",
+    },
+  ];
+
   return (
     <div className="widgetsContainer">
       <article>
+        {contenuWidget.map((widget) => (
+          <Widget
+            key={widget.name}
+            name={widget.name}
+            logo={widget.logo}
+            link={widget.link}
+            image={widget.image}
+          />
+        ))}
+      </article>
+      {/* <article>
         <Widget />
       </article>
       <article>
@@ -14,10 +34,7 @@ export default function WidgetList() {
       </article>
       <article>
         <Widget />
-      </article>
-      <article>
-        <Widget />
-      </article>
+      </article> */}
     </div>
   );
 }
