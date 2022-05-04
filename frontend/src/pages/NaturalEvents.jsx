6609b6c456c88e3b6759cd6458ee77bc06e13953
FREDERIQUE DEMAS <frederique.demas73@gmail.com>
2022-05-04 14:55:27 +0200

    fix display counter + resizing globe image2

--- frontend/src/pages/NaturalEvents.jsx
+++ frontend/src/pages/NaturalEvents.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/Home.scss";
-import "../styles/NaturalEvents.scss";
 import Titre from "../components/Titre";
 import NaturalEventsPics from "../components/NaturalEvents/NaturalEventsPics";
 
 export default function Home() {
   return (
     <div>
-      <Titre className="natTitle" titre="Natural Events from space" />
+      <div>
+        <Titre titre="Natural Events from space" />
+      </div>
       <NaturalEventsPics />
     </div>
   );
