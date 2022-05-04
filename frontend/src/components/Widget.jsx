488795b48729a0c4dbb0178d8e1f93d96532fa2b
Lora Perrichon <loraperrichon@gmail.com>
2022-05-04 12:21:53 +0200

    affichage de l'image ok, reste le css

--- frontend/src/components/Widget.jsx
+++ frontend/src/components/Widget.jsx
@@ -7,12 +7,14 @@ export default function Widget({ logo, name, image, link }) {
   return (
     <div className="widget">
       <NavLink to={link}>
-        <div className="logoWidget">
-          <img src={logo} alt={name} />
+        <div className="textWidget">
+          <div className="logoWidget">
+            <img src={logo} alt={name} />
+          </div>
+          <h3>{name}</h3>
         </div>
-        <h3>{name}</h3>
         <div className="imageWidget">
-          <img src={image} alt={name} />
+          <img src={image} alt={name} className="image-widget" />
         </div>
       </NavLink>
     </div>
--- frontend/src/components/WidgetList.jsx
+++ frontend/src/components/WidgetList.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Widget from "./Widget";
 
 import "../styles/Widget.scss";
 
 export default function WidgetList() {
+  const [curiosityPic, setCuriosityPic] = useState([]);
+
+  const getCuriosityPic = (date) => {
+    let newDate = date;
+    if (date === undefined) {
+      newDate = new Date();
+    }
+    const dateString = `${newDate.getFullYear()}-${
+      newDate.getMonth() + 1
+    }-${newDate.getDay()}`;
+    axios
+      .get(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${dateString}&api_key=BRVlKgV3WbAF21CU0f7H8dnjzLyeKh5go7aQObNB`
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setCuriosityPic(data.photos[0].img_src);
+      });
+  };
+
+  useEffect(() => {
+    getCuriosityPic();
+  }, []);
+
   const contenuWidget = [
     {
-      name: "Epic",
+      name: "Curiosity",
       logo: "../src/assets/perseverance.png",
-      link: "/Epic",
-      image: "../src/assets/lion-logo.png",
+      link: "/Curiosity",
+      image: curiosityPic,
+    },
+    {
+      name: "Natural Events",
+      logo: "../src/assets/tornade.png",
+      link: "/NaturalEvents",
+      image: "../src/assets/image-widget-events.png",
     },
   ];
 
   return (
     <div className="widgetsContainer">
-      <article>
-        {contenuWidget.map((widget) => (
+      {contenuWidget.map((widget) => (
+        <article>
           <Widget
             key={widget.name}
             name={widget.name}
@@ -24,8 +55,8 @@ export default function WidgetList() {
             link={widget.link}
             image={widget.image}
           />
-        ))}
-      </article>
+        </article>
+      ))}
       {/* <article>
         <Widget />
       </article>
