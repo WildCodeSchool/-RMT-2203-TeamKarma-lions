ddc58b1ed6871fdb57586f209ec8f1fda38c2137
Lora Perrichon <loraperrichon@gmail.com>
2022-05-05 14:34:42 +0200

    css responsive ok

--- frontend/src/components/Widget.jsx
+++ frontend/src/components/Widget.jsx
@@ -7,14 +7,18 @@ export default function Widget({ logo, name, image, link }) {
   return (
     <div className="widget">
       <NavLink to={link}>
-        <div className="textWidget">
-          <div className="logoWidget">
-            <img src={logo} alt={name} className="imglogo" />
+        <div className="texteImgWidget">
+          <div className="textWidget">
+            <div className="textLogo">
+              <div className="logoWidget">
+                <img src={logo} alt={name} className="imglogo" />
+              </div>
+              <h3>{name}</h3>
+            </div>
           </div>
-          <h3>{name}</h3>
-        </div>
-        <div className="imageWidget">
-          <img src={image} alt={name} className="image-widget imglogo" />
+          <div className="imageWidget">
+            <img src={image} alt={name} className="image-widget" />
+          </div>
         </div>
       </NavLink>
     </div>
