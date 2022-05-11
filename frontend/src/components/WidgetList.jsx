e52c3e8b27c259789bc3ef3d782a7433f16fca41
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-05-11 10:10:59 +0200

    add lazzy laoding funfact  + css fix

--- frontend/src/components/WidgetList.jsx
+++ frontend/src/components/WidgetList.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import moment from "moment";
 import axios from "axios";
 import Widget from "./Widget";
-
 import "../styles/Widget.scss";
 
 export default function WidgetList() {
